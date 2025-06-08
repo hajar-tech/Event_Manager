50712975aa69b032181962277e8dde04968e8e6b
hajar zerhouni <hajar.zerhounipr@gmail.com>
2025-06-08 13:39:39 +0100

    reservation  added successfully

--- backend/src/main/java/com/example/EventManager/controller/ClientController.java
+++ backend/src/main/java/com/example/EventManager/controller/ClientController.java
@@ -1,6 +1,9 @@
 package com.example.EventManager.controller;
 
+import com.example.EventManager.dto.ReservationDto;
 import com.example.EventManager.entity.Reservation;
+import com.example.EventManager.mapper.ReservationMapper;
+import com.example.EventManager.service.implementationServices.ReservationImpl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -8,12 +11,30 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/client")
 public class ClientController {
+
+    private final ReservationImpl reservationImpl;
+    private final ReservationMapper reservationMapper;
+
+    public ClientController(ReservationImpl reservationImpl, ReservationMapper reservationMapper) {
+        this.reservationImpl = reservationImpl;
+        this.reservationMapper = reservationMapper;
+    }
+
     @PreAuthorize("hasAnyRole('CLIENT', 'ADMIN')")
     @GetMapping("/bonjour")
     public ResponseEntity<String> bonjour() {
         return ResponseEntity.ok("Bonjour in the client dashboard");
     }
-//    @PostMapping("/reserver-event")
+
+
+    @PreAuthorize("hasAnyRole('CLIENT', 'ADMIN')")
+   @PostMapping("/reserver-event")
+    public ResponseEntity<ReservationDto> createReservation(@RequestBody ReservationDto reservationdto) {
+        Reservation reservation = reservationMapper.toReservation(reservationdto);
+        Reservation saved = reservationImpl.addReservation(reservation);
+        return ResponseEntity.ok(reservationMapper.toDto(saved));
+   }
+
 //    public ResponseEntity<?> reserverEvent(@RequestBody Reservation reservation) {
 //        // logique pour réserver un événement
 //        return ResponseEntity.ok("Réservation effectuée");
--- backend/src/main/java/com/example/EventManager/mapper/ReservationMapper.java
+++ backend/src/main/java/com/example/EventManager/mapper/ReservationMapper.java
@@ -3,9 +3,15 @@ package com.example.EventManager.mapper;
 import com.example.EventManager.dto.ReservationDto;
 import com.example.EventManager.entity.Reservation;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ReservationMapper {
-    ReservationDto toDTO(Reservation reservation);
-    Reservation toEntity(ReservationDto dto);
+    @Mapping(source = "event.id", target = "eventId")
+    @Mapping(source = "user.id", target = "userId")
+    ReservationDto toDto(Reservation reservation);
+
+    @Mapping(target = "event.id", source = "eventId")
+    @Mapping(target = "user.id", source = "userId")
+    Reservation toReservation(ReservationDto dto);
 }
